943861e7c294cb30df542b8da7ca6145f9a1c276
erfaneh khanmohammadi <erfanehhh@gmail.com>
2021-07-10 23:45:33 +0430

    main page clash royal

---
+++ src/sample/ControllerMainPageClashRoyal.java
@@ -0,0 +1,52 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class ControllerMainPageClashRoyal {
+
+    @FXML
+    private ImageView battleDeck;
+
+    @FXML
+    private ImageView profile;
+
+    @FXML
+    private ImageView trainingCamp;
+
+    @FXML
+    private ImageView oneVoneTwoVtwo;
+
+    @FXML
+    private ImageView buttleHistory;
+
+
+    @FXML
+    void BattleDeck(MouseEvent event) {
+//        Main.set();
+    }
+
+    @FXML
+    void ButtleHistory(MouseEvent event) {
+//        Main.set();
+    }
+
+
+    @FXML
+    void OneVoneTwoVtwo(MouseEvent event) {
+//        Main.set();
+    }
+
+    @FXML
+    void Profile(MouseEvent event) {
+//        Main.set();
+    }
+
+    @FXML
+    void TrainingCamp(MouseEvent event) {
+//        Main.set();
+    }
+
+
+}
