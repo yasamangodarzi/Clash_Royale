66a92b7ea4290bcbfd32d02185aaffb52fb15e8b
erfaneh khanmohammadi <erfanehhh@gmail.com>
2021-07-11 11:02:51 +0430

    DetectProximityTargetCard method

--- src/Game/Playground.java
+++ src/Game/Playground.java
@@ -1,14 +1,11 @@
 package Game;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Playground extends CommonGameData{
 
     List<Integer> Space = new ArrayList<>();
-    Map<Integer, List<String>> GoalsDistance = new HashMap<>();
+    Map<Integer, List<Card>> GoalsDistanceCard = new HashMap<>();
 
 
     public Playground() {
@@ -22,5 +19,45 @@ public class Playground extends CommonGameData{
         // and another target will be identified
         //Of course, in my opinion, this method can be written in general and be in the Playground class
         // so that both the soldier and the tower can shoot at the enemy.
+    }
+
+    public List<Card> DetectProximityTargetCard(int RowOrigin, int ColumnOrigin, int Range,
+                                                int DestinationRow, int DestinationColumn,
+                                                Card CardName, String Condition) {
+        List<Card> goal = new ArrayList<>();
+        double distanceRes = 0;
+        double ResultRowDifference = Math.pow((DestinationRow - RowOrigin), 2);
+        double ResultColumnDifference = Math.pow((DestinationColumn - ColumnOrigin), 2);
+        double distance = Math.sqrt(((int) ResultRowDifference + (int) ResultColumnDifference));
+        if(Condition.equals("Card")) {
+            Speed speed = ((Soldier) CardName).getSpeed();
+            switch (speed) {
+                case FAST:
+                    distanceRes = distance / 2;
+                    break;
+                case MEDIUM:
+                    distanceRes = distance;
+                    break;
+                case SLOW:
+                    distanceRes = distance * 2;
+                    break;
+            }
+        }
+        if (distanceRes == Range){
+            Space.add((int) distanceRes);
+            GoalsDistanceCard.computeIfAbsent((int) distanceRes, k -> new ArrayList<>()).add(CardName);
+            Collections.sort(Space);
+            ArrayList<Integer> SpaceSort = new ArrayList<>();
+            for (int temp : Space) {
+                SpaceSort.add(temp);
+            }
+            goal.add(GoalsDistanceCard.get(SpaceSort.get(0)).get(0));
+            for (int i = 1; i < SpaceSort.size(); i++) {
+                if (SpaceSort.get(0).equals(SpaceSort.get(i))) {
+                    goal.add(GoalsDistanceCard.get(SpaceSort.get(0)).get(i));
+                }
+            }
+        }
+        return goal;
     }
 }
