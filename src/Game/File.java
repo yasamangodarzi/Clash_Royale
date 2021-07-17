b6505b010a56646b31801971cb7a42b4f0a47d33
erfaneh khanmohammadi <erfanehhh@gmail.com>
2021-07-17 12:59:29 +0430

    Battle History page and change DetectProximityTargetCard method

--- src/Game/File.java
+++ src/Game/File.java
@@ -1,14 +1,13 @@
 package Game;
 
-// import ir.huri.jcal.JalaliCalendar;
- //import ir.huri.jcal.JalaliCalendar;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class File {
     private String Level;
     private ArrayList<String> HandsOfCards;
-    private boolean ResultGame;
+    public boolean ResultGame;
 
     public File(String Level, boolean ResultGame, ArrayList<String>HandsOfCards){
         this.Level = Level;
@@ -19,17 +18,36 @@ public class File {
     public void CreatFilePlayer() throws FileNotFoundException {
         String theEndResultOfGame = null;
         FileUtils file = new FileUtils();
-
-          // JalaliCalendar jalaliCalendar = new JalaliCalendar();
-
-     
+        Date date = new Date();
      
         if(ResultGame)
             theEndResultOfGame = "Winner";
         else
             theEndResultOfGame = "Loser";
-       //  InformationPlayer information = new InformationPlayer(theEndResultOfGame, HandsOfCards, jalaliCalendar.toString());
+         InformationPlayer information = new InformationPlayer(theEndResultOfGame, HandsOfCards, date.toString());
         file.creatDirectory("F:/ClashRoyal");
-        //file.writeObject("F:/ClashRoyal/" + Level + ".txt", information);
+        file.writeObject("F:/ClashRoyal/" + Level + ".txt", information);
+    }
+
+
+    /**
+     * This method has the ability to return a list of directories in a specific address
+     * @param name name file
+     * @return
+     */
+    public ArrayList<String> listDirectory(String name) {
+        java.io.File file = null;
+        String[] paths;
+        ArrayList<String> ArrayPath = new ArrayList<>();
+        try {
+            file = new java.io.File(name);
+            paths = file.list();
+            for (String path : paths) {
+                ArrayPath.add(path);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return ArrayPath;
     }
 }
